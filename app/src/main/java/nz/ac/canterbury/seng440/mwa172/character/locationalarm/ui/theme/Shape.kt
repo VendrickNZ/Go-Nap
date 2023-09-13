f6da6d73f9b1a02b18f84bbe4973334f21f50fab
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-09-13 12:42:23 +1200

    set up project

---
+++ app/src/main/java/nz/ac/canterbury/seng440/mwa172/character/locationalarm/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package nz.ac.canterbury.seng440.mwa172.character.locationalarm.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
