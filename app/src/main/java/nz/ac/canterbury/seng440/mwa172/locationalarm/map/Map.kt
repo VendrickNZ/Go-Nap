b5b1e474a4c90005be9ead4f273c1f44996e7791
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-10-05 17:38:21 +1300

    add crosshair to map isntead of weird marker pin

--- app/src/main/java/nz/ac/canterbury/seng440/mwa172/locationalarm/map/Map.kt
+++ app/src/main/java/nz/ac/canterbury/seng440/mwa172/locationalarm/map/Map.kt
@@ -3,16 +3,15 @@ package nz.ac.canterbury.seng440.mwa172.locationalarm.map
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import com.google.android.gms.location.FusedLocationProviderClient
+import androidx.compose.ui.res.painterResource
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
+import nz.ac.canterbury.seng440.mwa172.locationalarm.R
 
 @Composable
 @SuppressLint("MissingPermission")
@@ -25,7 +24,7 @@ fun AlarmMap(
         CameraPosition(LatLng(-43.5321, 172.6362), 20f, 0f, 0f)  // Default to Christchurch, NZ
     }
 
-    val liveLocation by viewModel
+    val liveLocation: LatLng by viewModel
         .location
         .observeAsState(initial = LatLng(-43.5321, 172.6362))  // Default to Christchurch, NZ
 
@@ -38,8 +37,13 @@ fun AlarmMap(
         modifier = modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
     ) {
-        Marker(
+        MarkerComposable(
             state = MarkerState(position = liveLocation)
-        )
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.crosshair),
+                contentDescription = ""
+            )
+        }
     }
 }
