6a496a81b31bfb06e916398a46c09d0bd8ea23b4
TheDeathlyCow <thedeathlycow@gmail.com>
2023-10-10 23:42:18 +1300

    show alarms on map

--- app/src/main/java/nz/ac/canterbury/seng440/mwa172/locationalarm/map/Map.kt
+++ app/src/main/java/nz/ac/canterbury/seng440/mwa172/locationalarm/map/Map.kt
@@ -5,8 +5,11 @@ import android.location.Location
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Switch
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Alarm
 import androidx.compose.material.icons.filled.PersonPin
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
@@ -21,11 +25,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Circle
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MarkerComposable
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import nz.ac.canterbury.seng440.mwa172.locationalarm.GoNapViewModel
+import nz.ac.canterbury.seng440.mwa172.locationalarm.alarm.Alarm
 import nz.ac.canterbury.seng440.mwa172.locationalarm.alarm.CreateAlarm
 import nz.ac.canterbury.seng440.mwa172.locationalarm.asLatLng
 
@@ -96,5 +102,62 @@ fun AlarmMap(
                 contentDescription = ""
             )
         }
+
+        Alarms(viewModel = viewModel)
     }
+}
+
+
+@Composable
+fun Alarms(
+    viewModel: GoNapViewModel
+) {
+    val userLocation: Location? by viewModel.location.observeAsState(initial = null)
+
+    if (userLocation == null) {
+        return
+    }
+
+    val alarms: List<Alarm> by viewModel.alarms.observeAsState(initial = listOf())
+
+    val distance = floatArrayOf(3f)
+    Log.d("AlarmView", "Placing ${alarms.size} alarms")
+
+    for (alarm in alarms) {
+        Location.distanceBetween(
+            userLocation?.latitude ?: 0.0,
+            userLocation?.longitude ?: 0.0,
+            alarm.latitude,
+            alarm.longitude,
+            distance
+        )
+
+        // this is bizarre
+        if (distance[0] <= 10_000f) {
+            AlarmView(alarm = alarm)
+        }
+    }
+}
+
+@Composable
+fun AlarmView(
+    alarm: Alarm
+) {
+    Log.d("AlarmView", "Placing alarm marker at ${alarm.latitude}, ${alarm.longitude}")
+    val alarmLocation: LatLng = alarm.location
+    MarkerComposable(
+        state = MarkerState(position = alarmLocation)
+    ) {
+        Image(
+            modifier = Modifier.size(64.dp),
+            imageVector = Icons.Filled.Alarm,
+            contentDescription = ""
+        )
+    }
+
+    Circle(
+        center = alarmLocation,
+        radius = alarm.radius,
+        fillColor = MaterialTheme.colors.secondary
+    )
 }
