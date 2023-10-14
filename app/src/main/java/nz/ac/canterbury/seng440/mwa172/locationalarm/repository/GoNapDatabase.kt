b93b104e06b585c88f95495b316fd92e98285cd5
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-10-14 15:29:29 +1300

    save settings in file

--- app/src/main/java/nz/ac/canterbury/seng440/mwa172/locationalarm/repository/GoNapDatabase.kt
+++ app/src/main/java/nz/ac/canterbury/seng440/mwa172/locationalarm/repository/GoNapDatabase.kt
@@ -8,7 +8,7 @@ import nz.ac.canterbury.seng440.mwa172.locationalarm.alarm.Alarm
 import nz.ac.canterbury.seng440.mwa172.locationalarm.alarm.AlarmDao
 import nz.ac.canterbury.seng440.mwa172.locationalarm.settings.Settings
 
-@Database(entities = [Alarm::class], version = 3)
+@Database(entities = [Alarm::class], version = 4)
 abstract class GoNapDatabase : RoomDatabase() {
 
     abstract fun alarmDao(): AlarmDao
